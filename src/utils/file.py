a393dd6e4c51c0ec30a808802b8f5102c85816da
afieldKG <afield@konradgroup.com>
2022-03-12 13:15:04 -0800

    feat('fix spacing')

--- src/utils/file.py
+++ src/utils/file.py
@@ -8,48 +8,48 @@ CSV_EXTENSION = '.csv'
 CSV_DELEMITER = ','
 
 def read_file(path):
-	_, file_extension = os.path.splitext(path)
+  _, file_extension = os.path.splitext(path)
 
-	try:
-		file = open(path, mode='r')
-	except OSError:
-		print(f"Could not open file: {path}")
-		raise
-	
-	data = {}
+  try:
+    file = open(path, mode='r')
+  except OSError:
+    print(f"Could not open file: {path}")
+  raise
 
-	with file:
-		if (file_extension == JSON_EXTENSION):
-			data = json.load(file)
-			return data
-		elif (file_extension == CSV_EXTENSION):
-			data = csv.DictReader(file, delimiter=CSV_DELEMITER)
-			return data
-		else:
-			raise Exception(f"Reading file extension: {file_extension} is not supported")
+  data = {}
+
+  with file:
+    if (file_extension == JSON_EXTENSION):
+      data = json.load(file)
+      return data
+    elif (file_extension == CSV_EXTENSION):
+      data = csv.DictReader(file, delimiter=CSV_DELEMITER)
+      return data
+    else:
+      raise Exception(f"Reading file extension: {file_extension} is not supported")
 
 def write_to_csv(path, rows):
-	try:
-		file = open(path, mode='w')
-	except OSError:
-		print("Could not open file: {path}")
-		raise
-	
-	if (len(rows) == 0): return
+  try:
+    file = open(path, mode='w')
+  except OSError:
+    print("Could not open file: {path}")
+    raise
 
-	with file:
-		fieldnames = list(rows[0].keys())
-		
-		print(f"writing to file: {file} with fieldnames: {fieldnames}")
-		writer = csv.DictWriter(file, fieldnames=fieldnames)
+  if (len(rows) == 0): return
 
-		failures = []
+  with file:
+    fieldnames = list(rows[0].keys())
 
-		for row in rows:
-			try:
-				writer.writerow(row)
-			except csv.Error:
-				failures.append
-				print(f"Error writing to csv for row: {csv.Error}")
-		
-		print(f"{len(rows)-len(failures)} of {len(rows)} rows inserted successfully")
+  print(f"writing to file: {file} with fieldnames: {fieldnames}")
+  writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+  failures = []
+
+  for row in rows:
+    try:
+      writer.writerow(row)
+    except csv.Error:
+      failures.append
+    print(f"Error writing to csv for row: {csv.Error}")
+
+    print(f"{len(rows)-len(failures)} of {len(rows)} rows inserted successfully")
